7dfebca6be602a2796c4c7a35d9a747859e91fbb
Exspiria <146676744+BaldurusExspa@users.noreply.github.com>
2024-05-27 08:02:59 +0300

    Add media (max-width: 560px)
    -Закончил media запрос на 1024px
    -Добавил media запрос на 560px (360 выпилил за ненадобность)
    -Мелкий баг фикс
    -Рефактор кода

--- js/fixedHeader.js
+++ js/fixedHeader.js
@@ -1,14 +1,25 @@
 window.addEventListener("scroll", function(){
     // console.log(this.pageYOffset)
 
-    const headerWrapper = document.getElementById("snp-header-wrapper");
-    const header = this.document.getElementById("snp-header");
+    const HeaderWrapper = this.document.getElementById("snp-header-wrapper");
+    const Header = this.document.getElementById("snp-header");
+    const Img = this.document.getElementById("snp-header-img");
 
     if (this.pageYOffset >= 449) {
-        headerWrapper.classList.add("fixed-snp-header-wrapper");
-        header.classList.add("fixed-snp-header");
+        HeaderWrapper.classList.add("fixed-snp-header-wrapper");
+        Header.classList.add("fixed-snp-header");
+        Img.setAttribute ('src', '../media/icons/YourTour_bl.svg');
     } else {
-        headerWrapper.classList.remove("fixed-snp-header-wrapper");
-        header.classList.remove("fixed-snp-header");
+        HeaderWrapper.classList.remove("fixed-snp-header-wrapper");
+        Header.classList.remove("fixed-snp-header");
+        Img.setAttribute ('src', '../media/icons/YourTour_wh.svg');
+    };
+
+    if (this.innerWidth <= 561) {
+        Img.setAttribute ('src', '../media/icons/YourTour_mb_wh.svg');
+    } else if (this.pageYOffset >= 449 && this.innerWidth <= 561) {
+        Img.setAttribute ('src', '../media/icons/YourTour_mb_bl.svg');
     }
+
+    // console.log(this.pageYOffset + ": Offset ____ " + this.innerWidth + ": Width")
 });
---
+++ js/selectImg.js
@@ -0,0 +1,6 @@
+const Select = document.getElementById('card-select');
+const Img = document.getElementById('selectImg');
+
+Img.addEventListener('click', function (e) {
+    Img.classList.add('selected')
+});
