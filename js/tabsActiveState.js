17bdf3bf07e99bd2b5a7e45b13fb401c6577de18
Exspiria <146676744+BaldurusExspa@users.noreply.github.com>
2024-07-18 02:20:43 +0300

    Refactor code - pro styles - media queries
    -Рефактор кода
    -Провел порядок стилей
    -Отредактировал media запросы
    -Заменил svg с файлов на html код
    -Заменил надписи alt с русского языка на английский язык
    -Мелкие фиксы

--- js/tabsActiveState.js
+++ js/tabsActiveState.js
@@ -3,10 +3,26 @@ const tabsButtons = document.querySelectorAll('.tab-btn');
 tabsButtons.forEach(btn => {
   btn.addEventListener('click', () => {
     const prevActiveButton = document.querySelector('.tab-btn.button-choosed');
+    const ActivePText = document.querySelector('.p.button-choosed')
     
     if (prevActiveButton) {
       prevActiveButton.classList.remove('button-choosed');
+      ActivePText.classList.remove('button-choosed');
     }
-    btn.classList.add('button-choosed');
+    btn.classList.add('button-choosed')
+    ActivePText.classList.add('button-choosed');
   });
 })
+
+// const tabsButtons = document.querySelectorAll('.tab-btn');
+
+// tabsButtons.forEach(btn => {
+//   btn.addEventListener('click', () => {
+//     const prevActiveButton = document.querySelector('.tab-btn.button-choosed');
+    
+//     if (prevActiveButton) {
+//       prevActiveButton.classList.remove('button-choosed');
+//     }
+//     btn.classList.add('button-choosed');
+//   });
+// })
