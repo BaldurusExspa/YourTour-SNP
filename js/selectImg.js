4c8174914d4e45a75840f11a6c813fd0f7bb38c1
Exspiria <146676744+BaldurusExspa@users.noreply.github.com>
2024-08-12 00:02:42 +0300

    Code refactor
    -Внёс требуемые правки по коду

--- js/selectImg.js
+++
@@ -1,8 +0,0 @@
-const Select = document.getElementById('card-select');
-const Img = document.getElementById('selectImg');
-
-
-
-Img.addEventListener('click', function (e) {
-    Img.classList.add('selected')
-});
