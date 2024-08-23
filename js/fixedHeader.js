f8180539cb786d731748e26c52b6d03e2b2602ef
Exspiria <146676744+BaldurusExspa@users.noreply.github.com>
2024-08-23 23:55:34 +0300

    Refactorclass names to suit the BEM methodology
    -Изменил названия классов и стили в соответствии с методикой БЭМ
    -Мелкий баг фикс

--- js/fixedHeader.js
+++ js/fixedHeader.js
@@ -1,11 +1,11 @@
-const HeaderWrapper = this.document.getElementById("snp-header-wrapper");
+const HeaderWrapper = this.document.getElementById("header-wrapper");
 
 window.addEventListener("scroll", function(){
     // console.log(this.pageYOffset)
 
     if (this.pageYOffset >= 449) {
-        HeaderWrapper.classList.add("fixed-snp-header-wrapper");
+        HeaderWrapper.classList.add("fixed-header-wrapper");
     } else {
-        HeaderWrapper.classList.remove("fixed-snp-header-wrapper");
+        HeaderWrapper.classList.remove("fixed-header-wrapper");
     };
 });
